f60564b90344fac9b994e85a9aa56402ab89925f
Kwang-Su Kim <atelier.jullian@gmail.com>
2019-12-05 23:47:58 +0000

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer">
+    <p>We can't wait to celebrate our special day with you.</p>
+  </div>
+</template>
+<style lang="scss">
+.footer {
+  align-self: flex-end;
+  height: 210px;
+}
+</style>
